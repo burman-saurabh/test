8672285d2dbca3f641d91be25593a00d6f2e5ed5
Your Name <you@example.com>
2019-02-01 01:33:33 +0530

    commit

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ console.log('hello world!!');
 
 function foo(){
     z = 321;
-<<<<<<< HEAD
-    y = 1;
-    x = 2;
-=======
     y = z;
->>>>>>> e0f1a60ab91e0e41554266165bed07abe7e71188
+    x = 2;
 }
